40f2f8349009839a4f4ddd93470056e79f7ecdcf
ojtdev <gabminer05@gmail.com>
2023-02-13 19:52:09 +0800

    auth guard working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AuthGuard } from './services/guard/auth.guard';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['landing']);
 
 const routes: Routes = [
   {
@@ -17,7 +21,8 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [ AuthGuard ]
+    canActivate: [ AngularFireAuthGuard ],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
     
   },
 ];
